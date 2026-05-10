41aea1af5bc167ffcaebee750423519196c1447c
kusumotoa <mshrksmt@gmail.com>
2026-05-11 02:06:16 +0900

    fix: align Package.swift with project requirements (Swift 6.1, macOS 15)

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
     name: "mimicry",
     platforms: [
-        .macOS(.v13)
-    ],
-    products: [
-        .executable(name: "mimicry", targets: ["mimicry"])
+        .macOS(.v15)
     ],
     targets: [
         .binaryTarget(
